80558cb5f2afeb7ccd335e590424b57177239028
Carlos Rafael <carlos.raafael@engenharia.ufjf.br>
2021-11-22 22:25:33 -0300

    tentando fazer o requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const pixelBox = document.getElementById('pixel-board');
-// para fazer esse desafio eu revi a aula 5.2 parte II
+
 for (let i = 0; i < 5; i += 1) {
   const line = document.createElement('div');
   pixelBox.appendChild(line);
-    for (let j = 0; j < 5; j += 1) {
-      const createBox = document.createElement('div');
-      pixelBox.appendChild(createBox);
-      createBox.className = 'pixel';
+  for (let j = 0; j < 5; j += 1) {
+    const createBox = document.createElement('div');
+    pixelBox.appendChild(createBox);
+    createBox.className = 'pixel';
   }
+}
+
+let clickColor = document.getElementById('color-palette');
+
+clickColor.addEventListener('click', selectColor)
+
+function selectColor(choose) {
+  
 }
